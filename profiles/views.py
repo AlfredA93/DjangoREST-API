0446ed22e559ff28b629d1774185eb7d3e44ffc2
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-08 11:24:11 +0200

    Add profiles detail view and edit functions

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,6 @@
+"""Profiles view functions"""
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -9,3 +12,30 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """Profile Detail"""
+    serializer_class = ProfileSerializer # This creates a form on the view
+    def get_object(self, pk):
+        """get profile object"""
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """get profile"""
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """Update Profile"""
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
