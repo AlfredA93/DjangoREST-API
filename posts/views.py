697a2b903c7ea6e48f5170ec362312689658e637
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-11 10:14:21 +0200

    Add PostDetail view for post CRUD functionality

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,10 @@
+# pylint: disable=C0103, E1101, W0707
+"""Post view functions"""
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
@@ -30,3 +34,43 @@ class PostList(APIView):
             return Response(
                 serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    """Post Detail"""
+
+    serializer_class = PostSerializer  # This creates a form on the view
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        """get post object"""
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """get post"""
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """Update post"""
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """Delete Post"""
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
