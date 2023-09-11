cb20ee9ee424ce180f1c33e190152589fef1844c
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-11 14:48:34 +0200

    Refactor posts and profile apps

--- posts/views.py
+++ posts/views.py
@@ -1,76 +1,99 @@
 # pylint: disable=C0103, E1101, W0707
 """Post view functions"""
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from django.http import Http404
+# from rest_framework import status, permissions
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(APIView):
-    """Post List API View"""
+class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
 
-    def get(self, request):
-        """Get List"""
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        """UpdateList"""
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class PostDetail(APIView):
-    """Post Detail"""
-
-    serializer_class = PostSerializer  # This creates a form on the view
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
 
-    def get_object(self, pk):
-        """get post object"""
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        """get post"""
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, context={'request': request})
-        return Response(serializer.data)
+# class PostList(APIView):
+#     """Post List API View"""
+#     serializer_class = PostSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly
+#     ]
 
-    def put(self, request, pk):
-        """Update post"""
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self, request):
+#         """Get List"""
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(
+#             posts, many=True, context={'request': request}
+#         )
+#         return Response(serializer.data)
 
-    def delete(self, request, pk):
-        """Delete Post"""
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+#     def post(self, request):
+#         """UpdateList"""
+#         serializer = PostSerializer(
+#             data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(
+#                 serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class PostDetail(APIView):
+#     """Post Detail"""
+
+#     serializer_class = PostSerializer  # This creates a form on the view
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         """get post object"""
+#         try:
+#             post = Post.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, post)
+#             return post
+#         except Post.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk):
+#         """get post"""
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, context={'request': request})
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         """Update post"""
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(
+#             post, data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         """Delete Post"""
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(
+#             status=status.HTTP_204_NO_CONTENT
+#         )
--- profiles/views.py
+++ profiles/views.py
@@ -1,51 +1,70 @@
 # pylint: disable=C0103, E1101, W0707
 """Profiles view functions"""
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from django.http import Http404
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+from rest_framework import generics
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
-    """View all Profiles"""
-    def get(self, request):
-        """get"""
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
-class ProfileDetail(APIView):
-    """Profile Detail"""
-
-    serializer_class = ProfileSerializer  # This creates a form on the view
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
-    def get_object(self, pk):
-        """get profile object"""
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        """get profile"""
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
+# class ProfileList(APIView):
+#     """View all Profiles"""
+#     def get(self, request):
+#         """get"""
+#         profiles = Profile.objects.all()
+#         serializer = ProfileSerializer(
+#             profiles, many=True, context={'request': request})
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        """Update Profile"""
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class ProfileDetail(APIView):
+#     """Profile Detail"""
+
+#     serializer_class = ProfileSerializer  # This creates a form on the view
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         """get profile object"""
+#         try:
+#             profile = Profile.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, profile)
+#             return profile
+#         except Profile.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk):
+#         """get profile"""
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(profile, context={'request': request})
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         """Update Profile"""
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(
+#             profile, data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
