5ea4b64333c3d2074501b5870788a1df0fa0a4f5
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-12 14:08:53 +0200

    Edit settings.py client origin and debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,8 +62,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('ALLOWED_HOSTS')]
 
